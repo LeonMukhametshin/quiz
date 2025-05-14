6331796ad6c622773682b0bf8e10f278a0d73c4b
LeonMukhametshin <leonmukhametshin2004@gmail.com>
2025-05-14 14:15:27 +0300

    style: обновлен интерфейс главной формы
    - Изменены отступы и выравнивание элементов
    - Подправлены размеры кнопок
    - Обновлены шрифты для лучшей читаемости

--- quiz/quiz/Form1.Designer.cs
+++ quiz/quiz/Form1.Designer.cs
@@ -28,33 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             radioButton1 = new RadioButton();
             radioButton2 = new RadioButton();
             radioButton3 = new RadioButton();
-            button1 = new Button();
-            button2 = new Button();
             radioButton4 = new RadioButton();
+            button1 = new CustomControls.RJControls.RJButton();
+            button2 = new CustomControls.RJControls.RJButton();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             label1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(12, 81);
             label1.Name = "label1";
-            label1.Size = new Size(120, 50);
+            label1.Size = new Size(1076, 103);
             label1.TabIndex = 0;
             label1.Text = "label1";
             label1.Click += label1_Click;
             // 
             // radioButton1
             // 
+            radioButton1.Anchor = AnchorStyles.Left;
             radioButton1.AutoSize = true;
-            radioButton1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            radioButton1.Location = new Point(12, 98);
+            radioButton1.Font = new Font("Segoe UI", 21.75F);
+            radioButton1.Location = new Point(12, 223);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(256, 54);
+            radioButton1.Size = new Size(203, 44);
             radioButton1.TabIndex = 1;
             radioButton1.TabStop = true;
             radioButton1.Text = "radioButton1";
@@ -63,11 +65,12 @@
             // 
             // radioButton2
             // 
+            radioButton2.Anchor = AnchorStyles.Left;
             radioButton2.AutoSize = true;
-            radioButton2.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            radioButton2.Location = new Point(12, 158);
+            radioButton2.Font = new Font("Segoe UI", 21.75F);
+            radioButton2.Location = new Point(12, 278);
             radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(256, 54);
+            radioButton2.Size = new Size(203, 44);
             radioButton2.TabIndex = 2;
             radioButton2.TabStop = true;
             radioButton2.Text = "radioButton2";
@@ -76,62 +79,90 @@
             // 
             // radioButton3
             // 
+            radioButton3.Anchor = AnchorStyles.Left;
             radioButton3.AutoSize = true;
-            radioButton3.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            radioButton3.Location = new Point(12, 218);
+            radioButton3.Font = new Font("Segoe UI", 21.75F);
+            radioButton3.Location = new Point(12, 333);
             radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(256, 54);
+            radioButton3.Size = new Size(203, 44);
             radioButton3.TabIndex = 3;
             radioButton3.TabStop = true;
             radioButton3.Text = "radioButton3";
             radioButton3.UseVisualStyleBackColor = true;
             radioButton3.CheckedChanged += radioButton3_CheckedChanged;
             // 
-            // button1
-            // 
-            button1.Location = new Point(93, 362);
-            button1.Name = "button1";
-            button1.Size = new Size(175, 72);
-            button1.TabIndex = 4;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(432, 362);
-            button2.Name = "button2";
-            button2.Size = new Size(175, 72);
-            button2.TabIndex = 5;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
             // radioButton4
             // 
+            radioButton4.Anchor = AnchorStyles.Left;
             radioButton4.AutoSize = true;
-            radioButton4.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            radioButton4.Location = new Point(12, 278);
+            radioButton4.Font = new Font("Segoe UI", 21.75F);
+            radioButton4.Location = new Point(12, 388);
             radioButton4.Name = "radioButton4";
-            radioButton4.Size = new Size(256, 54);
+            radioButton4.Size = new Size(203, 44);
             radioButton4.TabIndex = 6;
             radioButton4.TabStop = true;
             radioButton4.Text = "radioButton4";
             radioButton4.UseVisualStyleBackColor = true;
+            radioButton4.CheckedChanged += radioButton4_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.BackColor = SystemColors.MenuHighlight;
+            button1.BackgroundColor = SystemColors.MenuHighlight;
+            button1.BorderColor = Color.MediumBlue;
+            button1.BorderRadius = 10;
+            button1.BorderSize = 1;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Symbol", 15.75F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(150, 505);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 62);
+            button1.TabIndex = 7;
+            button1.Text = "Следующий";
+            button1.TextColor = Color.White;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Bottom;
+            button2.BackColor = SystemColors.MenuHighlight;
+            button2.BackgroundColor = SystemColors.MenuHighlight;
+            button2.BorderColor = Color.MediumBlue;
+            button2.BorderRadius = 10;
+            button2.BorderSize = 1;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Symbol", 15.75F, FontStyle.Bold);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(670, 505);
+            button2.Name = "button2";
+            button2.Size = new Size(250, 62);
+            button2.TabIndex = 0;
+            button2.Text = "Выход";
+            button2.TextColor = Color.White;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(659, 446);
-            Controls.Add(radioButton4);
+            ClientSize = new Size(1100, 600);
             Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(radioButton4);
             Controls.Add(radioButton3);
             Controls.Add(radioButton2);
             Controls.Add(radioButton1);
             Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(1100, 600);
+            MinimumSize = new Size(1100, 600);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterParent;
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
@@ -144,8 +175,8 @@
         private RadioButton radioButton1;
         private RadioButton radioButton2;
         private RadioButton radioButton3;
-        private Button button1;
-        private Button button2;
         private RadioButton radioButton4;
+        private CustomControls.RJControls.RJButton button1;
+        private CustomControls.RJControls.RJButton button2;
     }
 }
--- quiz/quiz/Form1.cs
+++ quiz/quiz/Form1.cs
@@ -1,8 +1,10 @@
+using MaterialSkin.Controls;
+using MaterialSkin;
 using System.Text;
 
 namespace quiz
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         private int questionCount;
         private int correctAnswers;
@@ -18,6 +20,11 @@ namespace quiz
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue400, Primary.Blue800, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
 
         private void Start()
@@ -93,7 +100,24 @@ namespace quiz
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "Следующий";
+            button2.Text = "Выход";
+
+            radioButton1.CheckedChanged += new EventHandler(StateOfSwitch);
+            radioButton2.CheckedChanged += new EventHandler(StateOfSwitch);
+            radioButton3.CheckedChanged += new EventHandler(StateOfSwitch);
+            radioButton4.CheckedChanged += new EventHandler(StateOfSwitch);
+            Start();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
             if (selecetResponse == correctAnswersNumber) correctAnswers++;
 
@@ -103,16 +127,16 @@ namespace quiz
 
                 questions[wrongAnswers] = label1.Text;
             }
-            if (button1.Text == "Начать тестирование сначала")
+            if (button1.Text == "Заного")
             {
-                button1.Text = "Следующий вопрос";
+                button1.Text = "Следующий";
 
                 radioButton1.Visible = true;
                 radioButton2.Visible = true;
                 radioButton3.Visible = true;
                 radioButton4.Visible = true;
 
-                Start(); 
+                Start();
                 return;
             }
 
@@ -130,7 +154,7 @@ namespace quiz
                     "Набранные балы: {2:F2}.", correctAnswers,
                     questionCount, (correctAnswers * 5.0F) / questionCount);
 
-                button1.Text = "Начать тестирование сначала";
+                button1.Text = "Заного";
 
                 var Str = "Список ошибок " + ":\n\n";
                 for (int i = 1; i <= wrongAnswers; i++)
@@ -140,23 +164,12 @@ namespace quiz
 
                 if (wrongAnswers != 0) MessageBox.Show(Str, "Тестирование завершено");
             }
-            if (button1.Text == "Следующий вопрос") Question();
+            if (button1.Text == "Следующий") Question();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            button1.Text = "Следующий вопрос";
-            button2.Text = "Выход";
-
-            radioButton1.CheckedChanged += new EventHandler(StateOfSwitch);
-            radioButton2.CheckedChanged += new EventHandler(StateOfSwitch);
-            radioButton3.CheckedChanged += new EventHandler(StateOfSwitch);
-            Start();
         }
     }
 }
